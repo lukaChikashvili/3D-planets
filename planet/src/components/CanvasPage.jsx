9a9cf68c3cd31605bf79a5738ce66e881913f811
luka chikashvili <lukachikashvili2020@gmail.com>
2024-09-16 13:47:25 +0400

    add setup of three js and glsl

---
+++ planet/src/components/CanvasPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Canvas } from '@react-three/fiber'
+
+const CanvasPage = () => {
+  return (
+  <>
+   <Canvas>
+        
+   </Canvas>
+  </>
+  )
+}
+
+export default CanvasPage
