18f097580b8915ddfe4c761361e89fb252bf1956
luka chikashvili <lukachikashvili2020@gmail.com>
2024-09-16 14:28:48 +0400

    add earth texture

--- planet/src/components/Main.jsx
+++ planet/src/components/Main.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
-import { OrbitControls} from '@react-three/drei'
+import React, { useRef } from 'react'
+import { OrbitControls, useTexture} from '@react-three/drei'
 import vertex from '../shaders/vertex.glsl';
 import fragment from '../shaders/fragment.glsl';
+import { SRGBColorSpace } from 'three';
+import * as THREE from 'three'
 
 
 
 
 const Main = () => {
 
+// load textures
+const dayTexture = useTexture('./day.jpg');
+dayTexture.colorSpace = THREE.SRGBColorSpace;
+const nightTexture = useTexture('./night.jpg');
+nightTexture.colorSpace = THREE.SRGBColorSpace;
+
+
+// uniforms
+const uniforms = useRef({
+    day: new THREE.Uniform(dayTexture),
+    night: new THREE.Uniform(nightTexture)
+});
 
 
   return (
@@ -15,7 +29,11 @@ const Main = () => {
        <OrbitControls makeDefault />
         <mesh>
            <sphereGeometry args={[ 2, 64, 64]} />
-           <shaderMaterial vertexShader={vertex} fragmentShader={fragment}/>
+           <shaderMaterial 
+            vertexShader={vertex}
+            fragmentShader={fragment}
+            uniforms={uniforms.current} 
+            />
         </mesh>
      </>
   )
--- planet/src/shaders/fragment.glsl
+++ planet/src/shaders/fragment.glsl
@@ -1,12 +1,20 @@
 varying vec2 vUv;
 varying vec3 vNormal;
 varying vec3 vPosition;
+uniform sampler2D day;
+uniform sampler2D night;
+
 
 void main()
 {
     vec3 viewDirection = normalize(vPosition - cameraPosition);
     vec3 normal = normalize(vNormal);
-    vec3 color = vec3(vUv, 1.0);
+    vec3 color = vec3(0.0);
+
+    // day and night texture
+    vec3 dayColor = texture(day, vUv).rgb;
+    vec3 nightColor = texture(night, vUv).rgb;
+    color = dayColor;
 
     // Final color
     gl_FragColor = vec4(color, 1.0);
