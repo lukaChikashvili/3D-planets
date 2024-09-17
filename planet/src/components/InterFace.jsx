12037eca2aca599e37fb7cfc94cb122ed016af82
luka chikashvili <lukachikashvili2020@gmail.com>
2024-09-17 21:37:15 +0400

    add gsap reveal animation

--- planet/src/components/InterFace.jsx
+++ planet/src/components/InterFace.jsx
@@ -1,5 +1,6 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import sound from '../assets/effect.wav';
+import gsap from 'gsap'
 
 const InterFace = () => {
     const [audio] = useState(new Audio(sound));
@@ -19,6 +20,32 @@ const InterFace = () => {
             audioRef.current.currentTime = 0; 
         }
     };
+
+
+    // text reveal effect
+    useEffect(() => {
+        gsap.to('.title', {
+            opacity: 1,
+            clipPath: "polygon(0 0%, 100% 0%, 100% 100%, 0 100%)",
+            duration: 2,
+            ease: "power2.inOut",
+            
+        });
+
+      gsap.fromTo('.buttons button', {
+        opacity:0,
+        y: 20
+      }, {
+        opacity: 1,
+        y: 0,
+        duration: 1,
+        ease: "power2.out",
+        stagger: 0.1
+      })
+
+
+    }, [])
+  
 
     return (
         <div className='interface_buttons'>
