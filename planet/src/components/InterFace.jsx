521804fed4d991ce99b837fca7233ec86e3abee7
luka chikashvili <lukachikashvili2020@gmail.com>
2024-09-18 21:06:20 +0400

    add menu text animations

--- planet/src/components/InterFace.jsx
+++ planet/src/components/InterFace.jsx
@@ -7,6 +7,8 @@ const InterFace = () => {
     const audioRef = useRef(audio);
 
     const [selectedPlanet, setSelectedPlanet] = useState('Discover the planets');
+    const [showMenu, setShowMenu] = useState(false);
+
 
 
     // Play sound
@@ -47,17 +49,103 @@ const InterFace = () => {
         delay: 0.4
       });
 
+      
+
+      
+
+
     
 
 
     }, []);
 
 
+    useEffect(() => {
+
+          if(showMenu) {
+            gsap.set('.menu', {
+                opacity: 0,
+                clipPath: "polygon(0 100%, 100% 100%, 100% 100%, 0 100%)", // starting hidden state
+            });
+    
+            gsap.to('.menu', {
+                opacity: 1,
+                clipPath: "polygon(0 0%, 100% 0%, 100% 100%, 0 100%)",
+                duration: 2,
+                ease: 'power2.inOut',
+                delay: 0.8, 
+            });
+          }else {
+            gsap.fromTo('.buttons button', {
+                opacity:0,
+                y: 20
+              }, {
+                opacity: 1,
+                y: 0,
+                duration: 1,
+                ease: "power2.out",
+                stagger: 0.1,
+                delay: 0.4,
+                pointerEvents: "all"
+              });
+
+              setSelectedPlanet('Discover the planets');
+
+
+          // reset the title reveal effect
+              gsap.set('.title', {
+                opacity: 0,
+                clipPath: "polygon(0 50%, 100% 50%, 100% 50%, 0 50%)"
+               });
+        
+               gsap.to('.title', {
+                opacity: 1,
+                clipPath: "polygon(0 0%, 100% 0%, 100% 100%, 0 100%)",
+                duration: 2,
+                ease: "power2.inOut",
+                
+            });
+        
+          }
+      
+    }, [showMenu])
+
     const handleClick = (planet) => {
        setSelectedPlanet(planet);
 
-       
-    }
+       gsap.set('.title', {
+        opacity: 0,
+        clipPath: "polygon(0 50%, 100% 50%, 100% 50%, 0 50%)"
+       });
+
+       gsap.to('.title', {
+        opacity: 1,
+        clipPath: "polygon(0 0%, 100% 0%, 100% 100%, 0 100%)",
+        duration: 2,
+        ease: "power2.inOut",
+        
+    });
+
+
+    gsap.fromTo('.buttons button', {
+        opacity:1,
+        y: 0
+      }, {
+        opacity: 0,
+        y: 20,
+        pointerEvents: "none",
+        duration: 1,
+        ease: "power2.out",
+        stagger: 0.1,
+        delay: 0.4
+      });
+
+      setShowMenu(true);
+     
+  };
+  
+
+    
   
 
     return (
@@ -76,6 +164,8 @@ const InterFace = () => {
                     </button>
                 ))}
             </div>
+
+            {showMenu && <p className='menu' onClick={() => setShowMenu(false)}>Planets</p>}
         </div>
     );
 };
