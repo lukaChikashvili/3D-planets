b6bb18cd9210d812c606c2bc97caafe699b70a5d
luka chikashvili <lukachikashvili2020@gmail.com>
2024-09-23 15:13:25 +0400

    add all planet intro pages

--- planet/src/components/IntroPage.jsx
+++ planet/src/components/IntroPage.jsx
@@ -4,7 +4,7 @@ import { PlanetContext } from '../context/PlanetContext';
 
 const IntroPage = () => {
 
-    const { introText, mercury, venus, mars, juputer, saturn, uranus, neptune } = useContext(PlanetContext);
+    const { introText, mercury, venus, mars, jupiter, saturn, uranus, neptune } = useContext(PlanetContext);
 
    
     // animate intro page
@@ -88,6 +88,20 @@ const IntroPage = () => {
        Mars was named by the Romans for their god of war because its reddish color was reminiscent of blood. The Egyptians called it "Her Desher," meaning "the red one."
 
 Even today, it is frequently called the "Red Planet" because iron minerals in the Martian dirt oxidize, or rust, causing the surface to look red.
+       ` : jupiter ? `
+       jupiter's signature stripes and swirls are actually cold, windy clouds of ammonia and water, floating in an atmosphere of hydrogen and helium. The dark orange stripes are called belts, while the lighter bands are called zones, and they flow east and west in opposite directions. Jupiter’s iconic Great Red Spot is a giant storm bigger than Earth that has raged for hundreds of years.
+
+       The king of planets was named for Jupiter, king of the gods in Roman mythology. Most of its moons are also named for mythological characters, figures associated with Jupiter or his Greek counterpart, Zeus.
+       ` : saturn ? `
+       Like fellow gas giant Jupiter, Saturn is a massive ball made mostly of hydrogen and helium. Saturn is not the only planet to have rings, but none are as spectacular or as complex as Saturn's. Saturn also has dozens of moons.
+       ` : uranus ? `
+       Uranus is a very cold and windy world. The ice giant is surrounded by 13 faint rings and 28 small moons. Uranus rotates at a nearly 90-degree angle from the plane of its orbit. This unique tilt makes Uranus appear to spin sideways, orbiting the Sun like a rolling ball.
+
+       Uranus was the first planet found with the aid of a telescope. It was discovered in 1781 by astronomer William Herschel, although he originally thought it was either a comet or a star. It was two years later that the object was universally accepted as a new planet, in part because of observations by astronomer Johann Elert Bode.
+       ` : neptune ? `
+       Dark, cold, and whipped by supersonic winds, ice giant Neptune is more than 30 times as far from the Sun as Earth. Neptune is the only planet in our solar system not visible to the naked eye. In 2011 Neptune completed its first 165-year orbit since its discovery in 1846.
+
+       Neptune is so far from the Sun that high noon on the big blue planet would seem like dim twilight to us. The warm light we see here on our home planet is roughly 900 times as bright as sunlight on Neptune.
        ` : `Earth is the third planet from the Sun and the only astronomical object known to harbor life. 
        This is enabled by Earth being an ocean world, 
        the only one in the Solar System sustaining liquid surface water.
@@ -108,7 +122,20 @@ Even today, it is frequently called the "Red Planet" because iron minerals in th
 Venus is named for the ancient Roman goddess of love and beauty, who was known as Aphrodite to the ancient Greeks. Most features on Venus are named for women. It’s the only planet named after a female god.
             ` : mars ? `
             Mars was named by the ancient Romans for their god of war because its reddish color was reminiscent of blood. Other civilizations also named the planet for this attribute – for example, the Egyptians called it "Her Desher," meaning "the red one." Even today, it is frequently called the "Red Planet" because iron minerals in the Martian dirt oxidize, or rust, causing the surface to look red.
-            ` : `
+            ` : jupiter ?  `
+            Jupiter’s environment is probably not conducive to life as we know it. The temperatures, pressures, and materials that characterize this planet are most likely too extreme and volatile for organisms to adapt to.
+
+While planet Jupiter is an unlikely place for living things to take hold, the same is not true of some of its many moons. Europa is one of the likeliest places to find life elsewhere in our solar system.
+            ` : saturn ? `
+            The farthest planet from Earth discovered by the unaided human eye, Saturn has been known since ancient times. The planet is named for the Roman god of agriculture and wealth, who was also the father of Jupiter.
+            Saturn's environment is not conducive to life as we know it. The temperatures, pressures, and materials that characterize this planet are most likely too extreme and volatile for organisms to adapt to.
+
+While planet Saturn is an unlikely place for living things to take hold, the same is not true of some of its many moons. Satellites like Enceladus and Titan, home to internal oceans, could possibly support life.
+            ` : uranus ? `
+            William Herschel tried unsuccessfully to name his discovery Georgium Sidus after King George III. Instead, the planet was named for Uranus, the Greek god of the sky, as suggested by Johann Bode.
+            
+            ` : neptune ? `The ice giant Neptune was the first planet located through mathematical calculations. Using predictions made by Urbain Le Verrier, Johann Galle discovered the planet in 1846. 
+            The planet is named after the Roman god of the sea, as suggested by Le Verrier.` : `
              Most of Earth's land is somewhat humid and covered by 
              vegetation, while large sheets of ice at Earth's polar deserts retain more water than Earth's 
              groundwater, lakes, rivers and atmospheric water combined. Earth's crust consists of slowly moving 
